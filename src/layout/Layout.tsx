5f4e85a91a9e41c7b55ddbacc6327100777998a4
Sebastian <zboru99@gmail.com>
2021-10-23 19:46:08 +0200

    add github link to navigation

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from 'react-router-dom'
 import DarkModeSwitch from "../components/navigation/DarkModeSwitch";
+import GithubLink from "../components/navigation/GithubLink";
 
 const Layout = (props: { children: React.ReactNode }): JSX.Element => {
     return (
@@ -9,7 +10,10 @@ const Layout = (props: { children: React.ReactNode }): JSX.Element => {
                 {/*Navigation*/}
                 <div className={'flex items-center px-5 justify-between md:px-0 pt-2 dark:text-white border-b border-gray-700 pb-2'}>
                     <Link to="/">Radioify dla Spotify</Link>
-                    <DarkModeSwitch/>
+                    <div className="flex">
+                        <GithubLink/>
+                        <DarkModeSwitch/>
+                    </div>
                 </div>
                 <div className="flex-grow sm:flex-grow-0">{props.children}</div>
             </div>
